cce223f9f4cf129c05c14530b76902c68c60f459
David Corey <dcorey@qualtrics.com>
2024-06-17 16:01:40 -0600

    Initial Commit
    Extract hadoop-snappy library and prepare for public release.

---
+++ snappy.go
@@ -0,0 +1,203 @@
+/*
+Package snappy implements decompression for the Hadoop format of snappy; a
+poorly documented compression scheme internal to the Hadoop ecosystem.
+
+The Hadoop format of snappy is similar to regular snappy block encoding,
+except that instead of compressing into one big block, Hadoop will create
+a stream of frames where each frame contains blocks that can each be
+independently decoded. A frame can contain 1 or more blocks and a stream
+can contain 1 or more frames.
+
+Each FRAME begins with a 4 byte header, which represents the total length
+of the frame after being DECOMPRESSED (i.e. once we're done decompressing
+the frame, this is how long the decompressed frame will be). This 4 byte
+header is a big endian encoded uint32. The header is not included in the
+total length of the frame.
+
+Each BLOCK in the frame also begins with a 4 byte header that is the
+COMPRESSED length of the block (i.e. how many bytes we need to read from
+the stream to get the entire block before we can decompress it). This
+header is also a big endian encoded uint32. The header is not included in
+the total length of the block.
+
+The stream structure is as follows
+
+	'['   == start of stream
+	']'   == end of stream
+	'|'   == component separator (symbolic only as the actual data has no padding or separators)
+	'...' == abbreviated
+
+	[ frame 1 header | block 1 header | block 1 | block 2 header | block 2 | ... | frame 2 header | block 1 header | block 1 | ... ]
+
+The format of each individual snappy block can be found here:
+
+	https://github.com/google/snappy/blob/main/format_description.txt
+*/
+package snappy
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/golang/snappy"
+)
+
+const (
+	// headerLength is the length in bytes of both the frame and block headers.
+	headerLength = 4
+)
+
+var (
+	errEmptyBlock           = errors.New("hadoop-snappy: zero length block in input stream")
+	errDecompressedTooLarge = errors.New("decompressed frame larger than expected")
+)
+
+// Reader wraps a hadoop-snappy compressed data stream and decompresses
+// the stream as it is read by the caller.
+type Reader struct {
+	// in is the compressed data stream we are reading from.
+	in io.Reader
+	// currentBlock is a block we have decompressed and are
+	// currently reading from.
+	currentBlock *bytes.Reader
+	// decompressedBuf is used simply as a mechanism to resize
+	// and reuse the underlying buffer as we are decompressing
+	// blocks, to avoid reallocating a slice unnecessarily.
+	decompressedBuf bytes.Buffer
+	// compressedBuf is used to read the compressed data from
+	// the input stream.
+	compressedBuf bytes.Buffer
+	// frameRemaining represents how many more uncompressed bytes
+	// we have left in the current frame.
+	frameRemaining int
+}
+
+// NewReader returns a Reader that can read the hadoop-snappy compressed format
+// that will be read from the io.Reader in. Reading from an input stream that is
+// not hadoop-snappy compressed will result in undefined behavior.
+func NewReader(in io.Reader) *Reader {
+	return &Reader{
+		in:           in,
+		currentBlock: bytes.NewReader([]byte{}),
+	}
+}
+
+// Read implements the io.Reader interface. Read will return the
+// decompressed data from the compressed input data stream. When
+// the input data stream returns io.EOF, indicating there is no
+// more data to read, Read will also return io.EOF indicating
+// that the stream has been fully read and decompressed.
+func (r *Reader) Read(out []byte) (int, error) {
+	if r.currentBlock.Len() > 0 {
+		return r.currentBlock.Read(out)
+	}
+
+	if r.frameRemaining == 0 {
+		err := r.nextFrame()
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	err := r.nextBlock()
+	if err != nil {
+		return 0, err
+	}
+
+	return r.currentBlock.Read(out)
+}
+
+func (r *Reader) nextFrame() error {
+	uncompressedSize, err := readHeader(r.in)
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			// The start of a new frame is the one spot we expect to reach EOF.
+			// To work correctly, Read must return EOF itself, not an error
+			// wrapping EOF, because callers will test for EOF using =="
+			return io.EOF
+		}
+
+		return fmt.Errorf("hadoop-snappy: read frame header: %w", err)
+	}
+
+	r.frameRemaining = uncompressedSize
+
+	return nil
+}
+
+func (r *Reader) nextBlock() error {
+	err := r.readNextBlock()
+	if err != nil {
+		return err
+	}
+
+	return r.decompress()
+}
+
+func (r *Reader) readNextBlock() error {
+	nextBlockLength, err := readHeader(r.in)
+	if err != nil {
+		if errors.Is(err, io.EOF) && r.frameRemaining != 0 {
+			err = io.ErrUnexpectedEOF
+		}
+
+		return fmt.Errorf("hadoop-snappy: read block header: %w", err)
+	}
+
+	if nextBlockLength == 0 && r.frameRemaining != 0 {
+		return errEmptyBlock
+	}
+
+	r.compressedBuf.Reset()
+	_, err = io.CopyN(&r.compressedBuf, r.in, int64(nextBlockLength))
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			// We should not encounter EOF here because we should have at least
+			// nextBlockLength left in the stream. If we encounter EOF here than
+			// the stream is malformed.
+			err = io.ErrUnexpectedEOF
+		}
+
+		return fmt.Errorf("hadoop-snappy: read block: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Reader) decompress() error {
+	decompressedLength, err := snappy.DecodedLen(r.compressedBuf.Bytes())
+	if err != nil {
+		return fmt.Errorf("hadoop-snappy: determine block decoded length: %w", err)
+	}
+
+	if decompressedLength > r.frameRemaining {
+		return fmt.Errorf("hadoop-snappy: decompress block: %w", errDecompressedTooLarge)
+	}
+
+	r.frameRemaining -= decompressedLength
+
+	r.decompressedBuf.Reset()
+	r.decompressedBuf.Grow(decompressedLength)
+
+	decompressed, err := snappy.Decode(r.decompressedBuf.Bytes(), r.compressedBuf.Bytes())
+	if err != nil {
+		return fmt.Errorf("hadoop-snappy: decompress block: %w", err)
+	}
+
+	r.currentBlock = bytes.NewReader(decompressed)
+
+	return nil
+}
+
+func readHeader(r io.Reader) (int, error) {
+	headerBuf := make([]byte, headerLength)
+	_, err := io.ReadFull(r, headerBuf)
+	if err != nil {
+		return 0, err
+	}
+
+	return int(binary.BigEndian.Uint32(headerBuf)), nil
+}
---
+++ snappy_test.go
@@ -0,0 +1,159 @@
+package snappy
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"io"
+	"os"
+	"testing"
+
+	gsnappy "github.com/golang/snappy"
+	"github.com/stretchr/testify/assert"
+)
+
+var errTestingError = errors.New("testing error")
+
+func TestSnappyReader(t *testing.T) {
+	tests := map[string]struct {
+		input          io.Reader
+		expectedOutput []byte
+		expectedErr    error
+	}{
+		"decompress hadoop compressed data": {
+			input:          bytes.NewReader(mustReadFile("testdata/test.jsonl.snappy")),
+			expectedOutput: mustReadFile("testdata/test.jsonl"),
+		},
+		"decompress large hadoop compressed data": {
+			input:          bytes.NewReader(mustReadFile("testdata/shakespeare.txt.snappy")),
+			expectedOutput: mustReadFile("testdata/shakespeare.txt"),
+		},
+		"decompress empty stream": {
+			input:          bytes.NewReader([]byte{}),
+			expectedOutput: []byte{},
+		},
+		"returns an error when decompressing truncated snappy data": {
+			input:       bytes.NewReader(mustReadFile("testdata/truncated.snappy")),
+			expectedErr: io.ErrUnexpectedEOF,
+		},
+		"returns an error when the frame size header is larger than the remaining stream": {
+			input: func() io.Reader {
+				data := mustReadFile("testdata/test.jsonl.snappy")
+				// data[0] is most significant byte in the frame header, which is 0x00 (0) in the unaltered file
+				// we should now be setting it to 0x01 (1)
+				data[0] += 1
+				return bytes.NewReader(data)
+			}(),
+			expectedErr: io.ErrUnexpectedEOF,
+		},
+		"returns an error when the frame size header is smaller than the actual frame size": {
+			input: func() io.Reader {
+				data := mustReadFile("testdata/test.jsonl.snappy")
+				// data[3] is least significant byte in the frame header, which is 0x0B (27) in the unaltered file
+				// we should now be setting it to 0x0A (26)
+				data[3] -= 1
+				return bytes.NewReader(data)
+			}(),
+			expectedErr: errDecompressedTooLarge,
+		},
+		"returns an error when the block size header is larger than the remaining stream": {
+			input: func() io.Reader {
+				data := mustReadFile("testdata/test.jsonl.snappy")
+				// data[4] is most significant byte in the block header, which is 0x00 (0) in the unaltered file
+				// we should now be setting it to 0x01 (1)
+				data[4] += 1
+				return bytes.NewReader(data)
+			}(),
+			expectedErr: io.ErrUnexpectedEOF,
+		},
+		"returns an error when the block size header is smaller than the actual block size": {
+			input: func() io.Reader {
+				data := mustReadFile("testdata/test.jsonl.snappy")
+				// data[7] is most significant byte in the block header, which is 0xE3 (227) in the unaltered file
+				// we should now be setting it to 0xE2 (226)
+				data[7] += 1
+				return bytes.NewReader(data)
+			}(),
+			expectedErr: io.ErrUnexpectedEOF,
+		},
+		"returns an error when it encounters a zero length block before reaching the end of the frame": {
+			input: bytes.NewReader([]byte{
+				// frame header of size 0xFF (255)
+				0x00, 0x00, 0x00, 0xFF,
+				// block header of size 0x00 (0)
+				0x00, 0x00, 0x00, 0x00,
+			}),
+			expectedErr: errEmptyBlock,
+		},
+		"returns an error when it encounters a corrupted snappy preamble": {
+			input: func() io.Reader {
+				data := mustReadFile("testdata/test.jsonl.snappy")
+				// data[8:9] are the snappy preamble for the test file's first block.
+				// it is the varint little endian representation of the block's uncompressed
+				// size. We will override the preamble making it too large for snappy to use.
+				data[8] = 0xFF
+				data[9] = 0xFF
+				data[10] = 0xFF
+				data[11] = 0xFF
+				return bytes.NewReader(data)
+			}(),
+			expectedErr: gsnappy.ErrCorrupt,
+		},
+		"returns an error when it encounters a correctly sized but corrupted snappy block": {
+			input: func() io.Reader {
+				data := mustReadFile("testdata/test.jsonl.snappy")
+				// Modify only the block of data, not the snappy preamble
+				blockSize := int(binary.BigEndian.Uint32(data[4:8]))
+				// we will skip the 2 bytes of the preamble
+				blockSize -= 2
+
+				// append non-snappy data after the snappy preamble
+				data = append(data[:10], bytes.Repeat([]byte{0xFF}, blockSize)...)
+				return bytes.NewReader(data)
+			}(),
+			expectedErr: gsnappy.ErrCorrupt,
+		},
+		"returns an error when the underlying reader returns an error": {
+			input: &errorReader{
+				err: errTestingError,
+			},
+			expectedErr: errTestingError,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			r := NewReader(test.input)
+			output, err := io.ReadAll(r)
+			if test.expectedErr == nil {
+				assert.Equal(t, test.expectedOutput, output, "output from snappy reader does not match expected")
+				assert.NoError(t, err, "unexpected error from snappy reader")
+			} else {
+				assert.ErrorIs(t, err, test.expectedErr, "error from snappy reader does not match expected")
+			}
+		})
+	}
+}
+
+func mustReadFile(filename string) []byte {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	return data
+}
+
+type errorReader struct {
+	err error
+}
+
+func (r *errorReader) Read(p []byte) (n int, err error) {
+	return 0, r.err
+}
