dea70b1fccc1fc48318d19368b960ba090a68345
David Corey <dcorey@qualtrics.com>
2024-07-17 13:57:15 -0600

    Move example to snappy_test package
    Move the package example to a _test package so that it better shows
    how to import the package.

--- example_test.go
+++ example_test.go
@@ -1,16 +1,18 @@
-package snappy
+package snappy_test
 
 import (
 	"bytes"
 	"fmt"
 	"io"
+
+	snappy "github.com/qualtrics/hadoop-snappy"
 )
 
 func Example() {
 	// encodedData is the string "Hello, world!" encoded using the hadoop-snappy compression format.
 	encodedData := []byte{0x00, 0x00, 0x00, 0x0D, 0x00, 0x00, 0x00, 0x0F, 0x0D, 0x30, 0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x2C, 0x20, 0x77, 0x6F, 0x72, 0x6C, 0x64, 0x21}
 
-	r := NewReader(bytes.NewReader(encodedData))
+	r := snappy.NewReader(bytes.NewReader(encodedData))
 
 	output, err := io.ReadAll(r)
 	if err != nil {
